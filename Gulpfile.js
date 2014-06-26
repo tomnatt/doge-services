e47322b9f9f0fe09d40cfc56cbab23bb9d470eb6
tomnatt <github@tomnatt.com>
2014-06-26 14:38:01 +0100

    added qunit

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var qunit = require('gulp-qunit');
 
 gulp.task('build', function() {
 
@@ -9,4 +10,9 @@ gulp.task('build', function() {
         .pipe(concat('dist/js/app.js'))
         .pipe(uglify())
         .pipe(gulp.dest('.'));
+});
+
+gulp.task('test', function() {
+    return gulp.src('./test/index.html')
+        .pipe(qunit());
 });

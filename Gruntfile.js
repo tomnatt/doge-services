6d3140dfa3fa0b36b91bdea01524bc4e060e4b43
tomnatt <github@tomnatt.com>
2014-06-26 11:13:43 +0100

    added connect for local serving

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,13 +12,26 @@ module.exports = function(grunt) {
                     "dist/js/app.js": ["js/lib/jquery.min.js", "js/accordian.js"]
                 }
             }
+        },
+
+        connect: {
+            server: {
+                options: {
+                    port: 3000,
+                    keepalive: true
+                }
+            }
         }
 
     });
 
     grunt.loadNpmTasks("grunt-contrib-uglify");
     grunt.loadNpmTasks("grunt-contrib-qunit");
+    grunt.loadNpmTasks("grunt-contrib-connect");
 
     // run these tasks in order, stopping if one fails
     grunt.registerTask("default", ["qunit", "uglify:build"]);
+
+    // this is an alias
+    grunt.registerTask("serve", ["connect"]);
 };

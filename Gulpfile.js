43f15ed2aeb90349dfa570e3f13f3c15a422d5f4
tomnatt <github@tomnatt.com>
2014-06-26 15:02:46 +0100

    added sass compilation, renamed uglify task and added a build task

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,15 +8,30 @@ var concat = require('gulp-concat');
 var qunit = require('gulp-qunit');
 */
 
-gulp.task('build', function() {
-
+gulp.task('uglify', function() {
     return gulp.src(['js/lib/*.js', 'js/*.js'])
         .pipe(tasks.concat('dist/js/app.js'))
         .pipe(tasks.uglify())
         .pipe(gulp.dest('.'));
 });
 
+gulp.task('sass', function () {
+    return gulp.src('sass/style.sass')
+        .pipe(tasks.rubySass({sourcemap: true}))
+        .pipe(gulp.dest('dist/css'));
+});
+
 gulp.task('test', function() {
     return gulp.src('./test/index.html')
         .pipe(tasks.qunit());
 });
+
+gulp.task('connect', function() {
+    tasks.connect.server({
+        root: '.',
+        port: 3000
+    });
+});
+
+gulp.task('serve', ['connect']);
+gulp.task('build', ['uglify', 'sass']);

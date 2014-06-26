f0d6ea32d889cdc6e9458532213a915fd009bfb9
tomnatt <github@tomnatt.com>
2014-06-26 14:35:09 +0100

    gulpified this project

---
+++ Gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+gulp.task('build', function() {
+
+    return gulp.src(['js/lib/*.js', 'js/*.js'])
+        .pipe(concat('dist/js/app.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('.'));
+});

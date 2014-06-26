68a208f67b449663096a1dc4bdd7ede08bb9d4ee
tomnatt <github@tomnatt.com>
2014-06-26 14:43:15 +0100

    added the automatic plugin loader

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,18 +1,22 @@
 var gulp = require('gulp');
 
+var tasks = require('gulp-load-plugins')();
+
+/* commented as we're using the task loader
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var qunit = require('gulp-qunit');
+*/
 
 gulp.task('build', function() {
 
     return gulp.src(['js/lib/*.js', 'js/*.js'])
-        .pipe(concat('dist/js/app.js'))
-        .pipe(uglify())
+        .pipe(tasks.concat('dist/js/app.js'))
+        .pipe(tasks.uglify())
         .pipe(gulp.dest('.'));
 });
 
 gulp.task('test', function() {
     return gulp.src('./test/index.html')
-        .pipe(qunit());
+        .pipe(tasks.qunit());
 });
